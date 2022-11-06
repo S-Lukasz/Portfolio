02d99f7bcf584faac11ff55c3f312046051a825d
lsurma <lukaszsurma73@gmail.com>
2022-11-06 21:44:53 +0100

    project button fix

--- components/ProjectButton.tsx
+++ components/ProjectButton.tsx
@@ -6,12 +6,10 @@ export default function Project(props: {
 
   return (
     
-    <button className="border pb-1 px-4 text-2xl text-orange-500 hover:bg-slate-800 hover:text-orange-300 group">
-        <a className="flex gap-2 items-center" href={"#"+props.index}>
-            <img className="w-6 h-6 opacity-75 group-hover:opacity-100" src={props.icon} alt='icon'/>
-            <p>{props.title}</p>
-        </a> 
-    </button>
+    <a className="border pb-1 px-4 text-2xl text-orange-500 hover:bg-slate-800 hover:text-orange-300 group flex gap-2 items-center" href={"#"+props.index}>
+        <img className="w-6 h-6 opacity-75 group-hover:opacity-100" src={props.icon} alt='icon'/>
+        <p>{props.title}</p>
+    </a>
 
   );
   
